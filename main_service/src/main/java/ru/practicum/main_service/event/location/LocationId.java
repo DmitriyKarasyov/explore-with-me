ed4033a3f97a2e19677eac65b3d56a0198ef20bb
Nightcall <bini2@yandex.ru>
2023-08-12 19:07:50 +0300

    default constructor for LocationId

--- main_service/src/main/java/ru/practicum/main_service/event/location/LocationId.java
+++ main_service/src/main/java/ru/practicum/main_service/event/location/LocationId.java
@@ -1,10 +1,15 @@
 package ru.practicum.main_service.event.location;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
 public class LocationId implements Serializable {
-    private final Float lat;
-    private final Float lon;
+    private Float lat;
+    private Float lon;
 
     public LocationId(Float lat, Float lon) {
         this.lat = lat;
