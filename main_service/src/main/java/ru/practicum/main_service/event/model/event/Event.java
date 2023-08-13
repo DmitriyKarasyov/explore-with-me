ccb0b8911d59ed8af4525b5a6ddff8c5f3b5b274
Nightcall <bini2@yandex.ru>
2023-08-13 16:50:49 +0300

    debug views

--- main_service/src/main/java/ru/practicum/main_service/event/model/event/Event.java
+++ main_service/src/main/java/ru/practicum/main_service/event/model/event/Event.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Cascade;
 import ru.practicum.main_service.category.model.Category;
 import ru.practicum.main_service.event.location.model.Location;
 import ru.practicum.main_service.event.model.state.State;
@@ -44,7 +43,7 @@ public class Event {
     @ManyToOne
     @JoinColumns({ @JoinColumn(name = "lat", referencedColumnName = "lat"),
             @JoinColumn(name = "lon", referencedColumnName = "lon") })
-    @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
+//    @Cascade(org.hibernate.annotations.CascadeType.PERSIST)
     private Location location;
     @Column(name = "paid", nullable = false)
     private Boolean paid;
